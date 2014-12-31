3bde4ab0ed96f26a2c439acc82b95417e1c44edf
AmmarHasan <hasanammar94@gmail.com>
2014-12-31 18:22:24 +0500

    Implement yieldreturn button logic

--- PracticingAlgorithmsAndDataStructures1/Form1.cs
+++ PracticingAlgorithmsAndDataStructures1/Form1.cs
@@ -16,5 +16,11 @@ namespace PracticingAlgorithmsAndDataStructures1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new YieldExample();
+        }
+
     }
 }

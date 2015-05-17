d0b64b83c4d00ce8c0c5d7ab8b3399a7f27c9b02
ammarhasan <hasanammar94@gmail.com>
2015-05-17 22:32:20 +0500

    my laptop commit

--- MyDataStructures/Program.cs
+++ MyDataStructures/Program.cs
@@ -10,6 +10,15 @@ namespace MyDataStructures
     {
         static void Main(string[] args)
         {
+            BinaryTree<int> b = new BinaryTree<int>();
+            b.Add(2);
+            b.Add(1);
+            b.Add(3);
+            b.Add(5);
+            b.Add(4);
+            DoublyNode<int> node3=b.Find(3);
+            DoublyNode<int> node5 = b.Find(5);
+            Console.Read();
         }
     }
 }
